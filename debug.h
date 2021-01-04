ecc2f6f12ed23ee1dcaae972d8d33034ec41aa63
Jonas Kuß <jonaskuss@protonmail.com>
2021-01-04 17:10:11 +0100

    Add stacktrace support to stderr output

--- debug.h
+++ debug.h
@@ -3,38 +3,57 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
+#include <execinfo.h>
 
-static char* dbg_identifier = "";
-static char* dbg_to_yellow = "\033[33;1;0m";
-static char* dbg_to_red = "\033[31;1;0m";
+char* dbg_identifier;
+static char* dbg_to_yellow = "\033[33;1m";
+static char* dbg_to_red = "\033[31;1m";
 static char* dbg_to_normal = "\033[0m";
+
+#define STACK_FRAME_SIZE 2
 
 static inline void debug(char* format, ...) {
 #ifdef DEBUG
     va_list args;
     va_start(args, format);
-    fprintf(stderr, "%s [*] %s: ", dbg_identifier, __func__);
+    fprintf(stderr, "%s [*] ", dbg_identifier);
     vfprintf(stderr, format, args);
     va_end(args);
 #endif
 }
 
 static inline void warn(char* format, ...) {
+    void* stackframes[STACK_FRAME_SIZE];
+    int n_traces = backtrace(stackframes, STACK_FRAME_SIZE);
     va_list args;
     va_start(args, format);
-    fprintf(stderr, "%s %s[WARNING]%s %s in %s:%d - ", dbg_identifier, dbg_to_yellow, dbg_to_normal, __func__, __FILE__, __LINE__);
+    char** symbols = backtrace_symbols(stackframes, STACK_FRAME_SIZE);
+    fprintf(stderr, "\n");
+    for (int i = 1; i < n_traces; i++) {
+        fprintf(stderr, "%s\n", symbols[i]);
+    }
+    fprintf(stderr, "%s[WARNING]%s[%s] ", dbg_to_yellow, dbg_to_normal, dbg_identifier);
     vfprintf(stderr, format, args);
     va_end(args);
+    free(symbols);
 }
 
 static inline void panic(char* format, ...) {
+    void* stackframes[STACK_FRAME_SIZE];
+    int n_traces = backtrace(stackframes, STACK_FRAME_SIZE);
     va_list args;
     va_start(args, format);
-    fprintf(stderr, "%s %s[PANIC]%s %s in %s:%d - ", dbg_identifier, dbg_to_red, dbg_to_normal, __func__, __FILE__, __LINE__);
+    char** symbols = backtrace_symbols(stackframes, STACK_FRAME_SIZE);
+    fprintf(stderr, "\n");
+    for (int i = 0; i < n_traces; i++) {
+        fprintf(stderr, "%s\n", symbols[i]);
+    }
+    fprintf(stderr, "%s[PANIC]%s[%s] ", dbg_to_red, dbg_to_normal, dbg_identifier);
     vfprintf(stderr, format, args);
     va_end(args);
-    exit(EXIT_FAILURE);
+    free(symbols);
 }
 
 #endif
