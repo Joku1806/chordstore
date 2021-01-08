5504a05ef0d6158acca38e9a8fd693baa6a8f338
Jonas Kuß <jonaskuss@protonmail.com>
2021-01-08 12:06:33 +0100

    fehlschlagenden Testcase aus der Testbench als setup benutzen

--- test_setup.sh
+++ test_setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-./build/peer 0     localhost 6500 65535 localhost 6503 21845 localhost 6501&
-./build/peer 21845 localhost 6501 0     localhost 6500 43690 localhost 6502&
-./build/peer 43690 localhost 6502 21845 localhost 6501 65535 localhost 6503&
-./build/peer 65535 localhost 6503 43690 localhost 6502 0     localhost 6500&
+./build/peer 1     localhost 6500 21607 localhost 6502 10    localhost 6501&
+./build/peer 10    localhost 6501 1     localhost 6500 21607 localhost 6502&
+./build/peer 21607 localhost 6502 10    localhost 6501 1     localhost 6500&
 
-cat test_value.txt | ./build/client localhost 6500 SET who\ you\ gonna\ call\?
+cat test_value.txt | ./build/client localhost 6501 SET The\ ciiirrcle\ of\ Chord\!
 killall peer
